bb488bab366712d6f1480609e70b48214a0127bf
AlekseyYarina <aleksey.yarina@gmail.com>
2024-02-15 11:03:44 +0200

    fix: generic

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,8 +3,14 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge (objA, objB) {
-  return Object.assign(objA, objB);
+// function merge(objA: object, objB: object) {
+//   return Object.assign({}, objA, objB);
+// }
+
+// export {};
+
+function merge<T, K>(objA: T, objB: K): T & K {
+  return Object.assign({}, objA, objB);
 }
 
-export {}
+export {};
