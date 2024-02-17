4f250838aa45850cdf0f91c6d7ba2173beb55cbf
AlekseyYarina <aleksey.yarina@gmail.com>
2024-02-17 16:30:00 +0200

    fix: generic 5

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,7 +3,12 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair<T extends object, K extends keyof T, V = T[K]> {
+// interface KeyValuePair<T extends object, K extends keyof T, V = T[K]> {
+//   key: K;
+//   value: V;
+// }
+
+interface KeyValuePair<K extends string, V extends any> {
   key: K;
   value: V;
 }
